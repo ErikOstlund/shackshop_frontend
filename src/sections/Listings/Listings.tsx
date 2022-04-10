f6d1283743c1cb0a81bcb29614ca9ab088b0d5e7
ErikOstlund <eostlund74@gmail.com>
2022-04-10 15:58:55 -0500

    Querying the Listings using fetch

--- src/sections/Listings/Listings.tsx
+++ src/sections/Listings/Listings.tsx
@@ -1,9 +1,37 @@
 import React from 'react';
+import { server } from '../../lib/api';
+
+const LISTINGS = `
+  query Listings {
+    listings {
+      id
+      title
+      image
+      address
+      price
+      numOfGuests
+      numOfBaths
+      numOfBeds
+      rating
+    }
+  }
+`;
 
 interface Props {
 	title: string;
 }
 
 export const Listings = ({ title }: Props) => {
-	return <h2>{title}</h2>;
+	const fetchListings = async () => {
+		const { data } = await server.fetch({ query: LISTINGS });
+
+		console.log(data);
+	};
+
+	return (
+		<div>
+			<h2>{title}</h2>
+			<button onClick={fetchListings}>Get Shacks</button>
+		</div>
+	);
 };
